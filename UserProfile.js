92d6666e79faea53b7a3d79fc9338ab0ab27083e
alishapaul01 <136269506+alishapaul01@users.noreply.github.com>
2023-08-28 19:30:10 +0530

    Final Porject

--- UserProfile.js
+++ UserProfile.js
@@ -1,11 +1,10 @@
 import classes from './UserProfile.module.css';
-import { useSelector } from 'react-redux';
 const UserProfile = () => {
-  const login= useSelector(state=> state.auth.isAuthenticated);
+  
 
   return (
     <main className={classes.profile}>
-      {login && <h2>My User Profile</h2>}
+    <h2>My User Profile</h2>
     </main>
   );
 };
